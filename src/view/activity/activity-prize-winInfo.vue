a0616239f08daabcc707fedfe8c3be3435db2cef
dongzhichao <dongzhichaogeek@163.com>
2019-12-23 08:33:55 +0800

    增加中奖管理页面

---
+++ src/view/activity/activity-prize-winInfo.vue
@@ -0,0 +1,242 @@
+<template>
+  <div>
+    <div class="search-con">
+      <Input v-model="listQuery.userPhone" clearable placeholder="用户"
+             class="search-item-first"/>
+      <Select v-model="listQuery.assignStatus" clearable placeholder="状态" style="width: 120px">
+        <Option v-for="item in assignStatusList" :value="item.label" :key="item.value">{{ item.value }}</Option>
+      </Select>
+      <Select v-model="listQuery.level" clearable placeholder="奖品等级" style="width: 120px">
+        <Option v-for="item in levelList" :value="item.label" :key="item.value">{{ item.value }}</Option>
+      </Select>
+      <Select v-model="listQuery.prizeType" clearable placeholder="奖品类型" style="width: 120px">
+        <Option v-for="item in prizeTypeList" :value="item.label" :key="item.value">{{ item.value }}</Option>
+      </Select>
+    </div>
+    <div class="search-con">
+      <DatePicker v-model="listQuery.winTimeRange"
+                  type="datetimerange"
+                  formart="yyyy-MM-dd"
+                  @on-change="listQuery.winTimeRange=$event"
+                  placement="bottom-end"
+                  placeholder="中奖时间范围"
+                  style="width: 300px"></DatePicker>
+      <DatePicker v-model="listQuery.assignTimeRange"
+                  type="datetimerange"
+                  formart="yyyy-MM-dd"
+                  @on-change="listQuery.assignTimeRange=$event"
+                  placement="bottom-end"
+                  placeholder="兑奖时间范围"
+                  style="width: 300px"></DatePicker>
+      <Button class="search-btn" type="primary" @click="getList" icon="md-search">搜索</Button>
+    </div>
+    <div>
+      <Table ref="prizeWintable" :data="actPrizeWinList" :columns="columns" :loading="listLoading" :border="true">
+        <template slot="level" slot-scope="scope">
+          {{levelMap[scope.row.level]}}
+        </template>
+        <template slot="prizeType" slot-scope="scope">
+          {{prizeTypeMap[scope.row.prizeType]}}
+        </template>
+        <template slot="assignStatus" slot-scope="scope">
+          {{assignStatusMap[scope.row.assignStatus]}}
+        </template>
+      </Table>
+      <Page v-show="total>0" :total="total" :current.sync="listQuery.current" :page-size="listQuery.size"
+            show-total show-sizer show-elevator
+            @on-change="getList" @on-page-size-change="handlePageSize"/>
+    </div>
+    <div>
+      <Row>
+        <Button style="float:right" type="primary" icon="md-download" :loading="exportLoading" @click="exportExcel">
+          导出excel
+        </Button>
+      </Row>
+    </div>
+  </div>
+</template>
+
+<script>
+  import excel from '@/libs/excel'
+  import {fetchList} from '@/api/activity/prizeWin'
+
+  export default {
+    name: 'activity-prize-winInfo',
+    data() {
+      return {
+        total: 10,
+        listLoading: false,
+        exportLoading: false,
+        actPrizeWinList: [],
+
+        listQuery: {
+          current: 1,
+          size: 10,
+          winTimeRange: [],
+          assignTimeRange: [],
+          userPhone: null,
+          assignStatus: null,
+          name: null,
+          level: null,
+        },
+        tempActPrizeWin: {
+          id: null,
+          userId: null,
+          actId: null,
+          prizeId: null,
+          winExt: null,
+          prizeWinExtExpress: null,
+          userPhone: null,
+          name: null,
+          level: null,
+          prizeType: null,
+          createTime: null,
+          assignStatus: null,
+          assignTime: null,
+          contactName: null,
+          contactPhone: null,
+          contactAddress: null,
+        },
+        columns: [
+          {
+            title: '中奖用户',
+            key: 'userPhone'
+          },
+          {
+            title: '奖品等级',
+            slot: 'level'
+          },
+          {
+            title: '奖品名称',
+            key: 'name'
+          },
+          {
+            title: '奖品类型',
+            slot: 'prizeType'
+          },
+          {
+            title: '中奖时间',
+            key: 'createTime'
+          },
+          {
+            title: '发放状态',
+            slot: 'assignStatus'
+          },
+          {
+            title: '兑奖时间',
+            key: 'assignTime'
+          }
+        ],
+        assignStatusList: [
+          {
+            value: '未发放',
+            label: 0
+          },
+          {
+            value: '待发放',
+            label: 1
+          },
+          {
+            value: '发放中',
+            label: 2
+          },
+          {
+            value: '发放成功',
+            label: 3
+          },
+          {
+            value: '发放失败',
+            label: 4
+          }
+        ],
+        levelList: [
+          {
+            value: '一等奖',
+            label: 1
+          },
+          {
+            value: '二等奖',
+            label: 2
+          },
+          {
+            value: '三等奖',
+            label: 3
+          }
+        ],
+        prizeTypeList: [
+          {
+            value: '未中奖',
+            label: 1
+          },
+          {
+            value: '虚拟物品',
+            label: 2
+          },
+          {
+            value: '邮寄实物',
+            label: 3
+          },
+          {
+            value: '线下处理物品',
+            label: 4
+          }
+        ],
+        levelMap: {
+          1: '一等奖',
+          2: '二等奖',
+          3: '三等奖'
+        },
+        prizeTypeMap: {
+          0: '未中奖',
+          1: '虚拟物品',
+          2: '邮寄实物',
+          3: '线下处理物品'
+        },
+        assignStatusMap: {
+          0: '未兑奖',
+          1: '待发放',
+          2: '发放中',
+          3: '发放成功',
+          4: '发放失败'
+        }
+      }
+    },
+    created() {
+      this.getList()
+    },
+    methods: {
+      getList() {
+        this.listLoading = true
+        fetchList(this.listQuery).then(response => {
+          this.actPrizeWinList = response.data.records
+          this.total = response.data.total
+          this.listLoading = false
+        })
+      },
+      handlePageSize(value) {
+        this.listQuery.size = value
+        this.getList()
+      },
+      exportExcel() {
+        if (this.actPrizeWinList.length) {
+          this.exportLoading = true
+          const params = {
+            title: ['中奖用户', '奖品等级', '奖品名称', '奖品类型', '中奖时间', '发放状态', '兑奖时间'],
+            key: ['userPhone', 'level', 'name', 'prizeType', 'createTime', 'assignStatus', 'assignTime'],
+            data: this.actPrizeWinList,
+            autoWidth: true,
+            filename: '中奖列表'
+          }
+          excel.export_array_to_excel(params)
+          this.exportLoading = false
+        } else {
+          this.$Message.info('表格数据不能为空！')
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
