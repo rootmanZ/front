7e5eb103cda544baa8e1c5d797f73f83a011a93a
dongzhichao <dongzhichaogeek@163.com>
2020-09-02 12:50:31 +0800

    二维码生成组件

---
+++ src/components/qrcode/qrcode.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+    <vue-qr :text="qrContent"
+            :size="200">
+    </vue-qr>
+
+  </div>
+</template>
+<script>
+import vueQr from 'vue-qr'
+
+export default {
+  components: {
+    vueQr
+  },
+  data () {
+    return {
+      // qrContent: "https://www.xxx.html"
+      qrContent: 'xmrbi'
+    }
+  },
+  created () {
+  },
+  methods: {}
+}
+
+</script>
